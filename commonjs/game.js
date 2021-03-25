1c6c368f4be5827de3c480fcadb6783419bc889e
samwangdd <wyysammore@gmail.com>
2021-03-25 23:22:58 +0800

    style: code clean

--- commonjs/game.js
+++ commonjs/game.js
@@ -16,17 +16,14 @@ module.exports = function (playerAction) {
 
   let point; // 电脑得分
   if (computerAction === playerAction) {
-    console.log('结果 :>> ', '平局');
     point = 0;
   } else if (
     (computerAction === 'rock' && playerAction === 'scissor') ||
     (computerAction === 'scissor' && playerAction == 'paper') ||
     (computerAction === 'paper' && playerAction == 'rock')
   ) {
-    console.log('结果 :>> ', '你输了');
     point = 1;
   } else {
-    console.log('结果 :>> ', '你赢了');
     point = -1;
   }
   return point;
