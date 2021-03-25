65be6cbeb2cfb0566071bede0e2a6b4c74791d50
王森 <wyysammore@gmail.com>
2021-03-25 23:14:15 +0800

    refactor: game

--- http/index.js
+++ http/index.js
@@ -1,16 +1,40 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url'); // 解析 URL
+const querystring = require('querystring'); // 将 string 解析为 object
+
+const game = require('../commonjs/game');
 
 http
-  .createServer((req, res) => {
-    if (res.url === '/favicon.ico') {
-      res.writeHead(200);
-      res.end();
+  .createServer((request, response) => {
+    const parseUrl = url.parse(request.url);
+    const { pathname, query } = parseUrl;
+    if (pathname === '/favicon.ico') {
+      response.writeHead(200);
+      response.end();
       return;
     }
 
-    res.writeHead(200);
-    // 输出网页
-    fs.createReadStream(__dirname + '/template.html').pipe(res);
+    if (pathname === '/game') {
+      const _query = querystring.parse(query);
+      const _action = _query.action;
+      const gameRes = game(_action);
+      console.log('gameRes :>> ', gameRes);
+      response.writeHead(200);
+
+      if (gameRes === 0) {
+        response.end('平局');
+      } else if (gameRes === 1) {
+        response.end('你输了！');
+      } else {
+        response.end('你赢了！');
+      }
+    }
+
+    if (pathname === '/') {
+      response.writeHead(200);
+      // 输出网页
+      fs.createReadStream(__dirname + '/index.html').pipe(response);
+    }
   })
   .listen(3000);
