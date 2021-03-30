84b9c940444bb87a0158e43dc6f5db63f9d6c174
samwangdd <wyysammore@gmail.com>
2021-03-30 22:01:46 +0800

    style: remove dead code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const game = require('./commonjs/game');
-const length = process.argv.length;
+// const length = process.argv.length;
 // const playerAction = process.argv[length - 1];
 
 // if (length < 3) {
@@ -9,13 +9,18 @@ const length = process.argv.length;
 
 let count = 0;
 process.stdin.on('data', e => {
+  // console.log('process.stdin.read() :>> ', process.stdin.read());
+  // const chunk = process.stdin.read();
+  // if (chunk) {
+  //   process.stdout.write('data >>  ' + chunk);
+  // }
   const playerAction = e.toString().trim();
   const result = game(playerAction);
-
-  if (result === -1) {
+  console.log('result :>> ', result);
+  if (result.point === -1) {
     count++;
   }
-
+  console.log('count :>> ', count);
   if (count >= 3) {
     console.log('-------------- :>> ');
     console.log('你太强了，我不玩了！');
