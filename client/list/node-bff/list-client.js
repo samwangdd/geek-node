846b1879ef383447ea3ec38d9ab7a8635f646da3
samwangdd <wyysammore@gmail.com>
2021-05-05 22:50:25 +0800

    feat: add list node-bff

---
+++ client/list/node-bff/list-client.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const protobuf = require('protocol-buffers');
+
+const appRoot = process.cwd();
+const schema = protobuf(fs.readFileSync(`${appRoot}/constant/list.proto`));
+const createSocket = require('../../util/createSocket');
+
+module.exports = createSocket({
+  port: 3006,
+  schema: schema,
+  schemaKey: ['ListRequest', 'ListResponse'],
+  timeout: 6000,
+});
