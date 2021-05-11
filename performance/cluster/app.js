57d9a8652e1f969cbfb38a601ef971e57e6b7a53
samwangdd <wyysammore@gmail.com>
2021-05-11 11:56:40 +0800

    feat: 守护进程

--- performance/cluster/app.js
+++ performance/cluster/app.js
@@ -3,9 +3,13 @@ const fs = require('fs');
 
 http
   .createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.end(fs.readFileSync(__dirname + '/index.htm', 'utf-8'));
+    setTimeout(() => {
+      res.writeHead(200, { 'content-type': 'text/html' });
+      res.end(fs.readFileSync(__dirname + '/index.htm', 'utf-8'));
+      // while (true) {} // 模拟程序死循环
+      // console.log('href :>> ', window.location.href); // 模拟程序崩溃
+    }, 500);
   })
   .listen(3000, () => {
-    console.log('listening :>> ', 3000);
+    // console.log('listening :>> ', 3000);
   });
